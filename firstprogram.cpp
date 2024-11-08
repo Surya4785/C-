63a060e64264439dfe0f4f8a60fcef9c09b9eaf1
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-09 02:55:46 +0530

    Add files via upload

---
+++ firstprogram.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    
+    std::cout << "Hello World " <<"\n";
+    std::cout << "This is my first program." <<std::endl;
+    std::cout << "This is the begining of my new journey.";
+    return 0;
+}
