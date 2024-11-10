8cb783ad8d6668383b001d2528d13fa9d1fcd7fb
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-10 19:35:21 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;   //decleration
+    x = 5;   //initialisation
+    int y;
+    y = 7;
+
+    cout<<x+y<<endl<<x-y<<endl<<x*y<<endl<<x+y-2;
+
+}
