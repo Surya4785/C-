45f6d967370073dbadca2c42fdca27342dc499c0
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-10 18:49:30 +0530

    Add files via upload

---
+++ output.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"My name is SURYA"<<endl<<"I am from Ballia, Uttar Pradesh."<<endl<<"I am a Btech student in ABES Engineering college Ghazibad, U.P";
+     
+}
