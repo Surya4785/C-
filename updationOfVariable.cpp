1fe5e13a50149f912648f8b3a04e076d9e4f0af8
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-10 19:35:43 +0530

    Add files via upload

---
+++ updationOfVariable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 5;
+    cout<<x<<endl;
+    x = 7;
+    cout<<x<<endl;
+    x = x + 6;     //x = 7 + 6
+    cout<<x<<endl;
+    x = x - 20;    //x = 13 - 20
+    cout<<x<<endl;
+}
