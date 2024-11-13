e61b8673e2721df7256f641f7ef5de1459033e1f
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-13 12:32:03 +0530

    Add files via upload

---
+++ incrementDecrement.cpp
@@ -0,0 +1,16 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    int x = 4;
+    cout<<x<<endl;
+    cout<<x++<<endl<<x;
+}*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+int y = 5;
+    cout<<y<<endl;
+    cout<<++y<<endl<<y;
+}
