f6e6705190956bafb6cd3e6a7e2256614527bbe7
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-13 12:31:08 +0530

    Add files via upload

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+   float r = 4.5;
+   //float Area = 3.1415*r*r;
+   //cout<<"Area is : ";
+   //cout<<Area<<endl;
+
+   //OR
+
+   cout<<"Area is : "; 
+   cout<<3.1415*r*r;  //Direct   
+}
