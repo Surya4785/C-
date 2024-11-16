43f38a91462d94c18321b3729cdfabd8d4a21956
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-16 23:22:10 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+//To print the sum of two numbers...
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x ,y;
+    cout<<"Enter the first number : ";
+    cin>>x;
+    cout<<"Enter the second number : ";
+    cin>>y;
+    cout<<"Sum of the numbers is :"<<x+y;
+}
