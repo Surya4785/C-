5e0196afe9a816c30629401af74c0d7b200beecc
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-16 23:22:47 +0530

    Add files via upload

---
+++ UserInput.cpp
@@ -0,0 +1,11 @@
+//To print the square of a number...
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number : ";
+    cin>>x;
+    cout<<"Square of this number is : ";
+    cout<<x*x;
+}
