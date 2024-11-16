05272e2d74a1844605c17d71024c2a27771c5344
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-16 23:38:20 +0530

    Add files via upload

---
+++ modulus.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 17;
+    int y = 3;
+    cout<<x%y;
+}
+    //modulus means remainder
+    // a%b = a (if a<b)
+    // a%a = 0
+    // a%(-b) = a%b
+    // (-a)%b = -[a%b]
