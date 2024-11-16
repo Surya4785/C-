623e3f52f15d468a70bc4888a1238bd169d9c2be
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-16 23:21:40 +0530

    Add files via upload

---
+++ Product.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,z;
+    cout<<"Enter the first number : ";
+    cin>>x;
+    cout<<"Enter the second nnumber : ";
+    cin>>y;
+    cout<<"Enter the third number : ";
+    cin>>z;
+    cout<<"The product of the given numbers is : "<<x*y*z;
+}
