a470ded9d515b69a636fd90fb67472dc4b98bc7c
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-11-25 23:57:57 +0530

    Add files via upload

---
+++ ReverseNum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    //variables initialization
+    int num, reverse = 0, rem;
+    cout <<"Enter the number :"<<endl; 
+    cin>>num;
+  
+    //loop to find reverse number
+    while(num != 0)
+    {
+      rem = num % 10;
+      reverse = reverse * 10 + rem;
+      num /= 10;
+    };
+ 
+    cout <<"\nReversed Number: "<<reverse;
+    
+    return 0;
+}
