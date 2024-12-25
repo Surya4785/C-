c8dfd3f0427e86d9ace1811486e5472c7da2dd11
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-12-26 02:54:18 +0530

    Update output.cpp

--- output.cpp
+++ output.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
-    cout<<"My name is SURYA"<<endl<<"I am from Ballia, Uttar Pradesh."<<endl<<"I am a Btech student in ABES Engineering college Ghazibad, U.P";
-     
+    cout<<"My name is SURYA"<<endl
+        <<"I am from Ballia, Uttar Pradesh."<<endl
+        <<"I am a Btech student in ABES Engineering college Ghazibad, U.P";    
 }
