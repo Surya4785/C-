d311ddb2e410246f4e2475b5918545b62a83fec9
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-01-11 03:19:08 +0530

    Add files via upload

---
+++ Quiz03.cpp
@@ -0,0 +1,12 @@
+// Take integer input and print the absolute value of that integer.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter an integer: ";
+    cin>>n;
+    if(n>=0) cout<<n;
+    else cout<<-n;
+}
